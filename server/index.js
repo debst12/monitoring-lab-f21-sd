32ba5e4eb7bd1a3db9ef2db398da18b7b82ce890
Spencer DeBrine <debst12@gmail.com>
2022-04-28 15:10:42 -0600

    added endpoint

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
 
 const port = process.env.PORT || 4545
 
