51110593908ff3cb9052fb00556b122a4bd67536
Spencer DeBrine <debst12@gmail.com>
2022-04-28 16:09:19 -0600

    added movie list functionallity

--- server/index.js
+++ server/index.js
@@ -41,6 +41,27 @@ app.get('/warning', () => {try{
     rollbar.warning(error)
 }})
 
+app.post('/api/movie', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
+
+
 const port = process.env.PORT || 4545
 
 app.use(rollbar.errorHandler())
